9f31651bf80b874dcbd03da3f777e96ace543425
BizonKrystof <147808998+BizonKrystof@users.noreply.github.com>
2023-11-21 10:42:44 +0100

    Add files via upload

---
+++ JavaScript_úkol/script.js
@@ -0,0 +1,24 @@
+let a = 0;
+let b = 0;
+
+let x = 1;
+let nasobek = 0;
+function zvysitA(){
+    a++;
+    console.log(a);
+}
+function zvysitB(){
+    b++;
+    console.log(b);
+}
+function vypsatCisla(){
+    nasobek = a*b
+    while (x<=nasobek) {
+        console.log(x);
+        x+=2;
+    }
+}
+function resetovat() {
+    a = 0;
+    b = 0;
+}
