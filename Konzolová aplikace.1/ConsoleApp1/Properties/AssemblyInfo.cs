032ccf2767ec6751b3a53a7812fd643444503ae6
BizonKrystof <147808998+BizonKrystof@users.noreply.github.com>
2024-04-05 11:06:00 +0200

    Add files via upload

---
+++ Konzolová aplikace.1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Tento GUID se používá pro ID knihovny typů, pokud je tento projekt COM.
+[assembly: Guid("8dfe19a2-19a9-4b0e-b818-f4b2c17307ca")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
