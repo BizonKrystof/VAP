cd9ff764e673a223fb6637461d1abcb33373456e
BizonKrystof <147808998+BizonKrystof@users.noreply.github.com>
2024-04-09 10:46:30 +0200

    Add files via upload

---
+++ ukol_konzole5/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Nedostatečný počet argumentů");
+                return;
+            }
+
+
+
+            string path = "soubor.txt";
+            if (File.Exists("path"))
+            {
+                string filename = args[0];
+                Dictionary<char, int> characterCounts;
+                StreamReader reader = new StreamReader(path);
+                using (StreamWriter writer = new StreamWriter(args[1], false))
+                {
+                    int lineCount = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        string text = reader.ReadLine();
+                        writer.WriteLine($"Znak{++lineCount}: {text}");
+                    }
+                }
+
+                reader.Close();
+            }
+            Console.Read();
+        }
+    }
+}
